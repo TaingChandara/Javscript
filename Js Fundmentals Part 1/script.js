324447e7b75e641c62a96bcf17ca47b5d383e524
unknown <Chandara.Taing@jtrustroyal.com>
2022-11-30 14:04:37 +0700

    first commit

---
+++ Js Fundmentals Part 1/script.js
@@ -0,0 +1,107 @@
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1999
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+
+// Coding challenge
+
+// const massdara = 78;
+// const heigthdara = 1.69;
+// const massjay = 92;
+// const heighjay = 1.95;
+
+// const massdara = 78;
+// const heighdara = 1.89;
+// const massjay = 85;
+// const heightjay = 1.76;
+
+// const BMIdara = massdara / heighdara ** 2;
+// const BMIjay = massjay / (heighjay * heighjay);
+// const daraheighBMI = BMIdara > BMIjay;
+// console.log(BMIdara, BMIjay, daraheighBMI);
+
+
+// String and Template
+
+// const firstname = 'dara';
+// const job = 'teacher';
+// const birthYear = 1991;
+
+
+// const dara = "i'm" + firstname + ',a' + (year - birthYear) + 'year old' + job + '!';
+// console.log(dara);
+
+// const daraNew = `i'm $(firstname), a $(year- birthYear) year old ${job}!`;
+// console.log(daraNew);
+
+// console.log(`Just a regular string...`);
+
+// console.log('String with \n\ multiple \n\ lines');
+
+// console.log('String multiple Lines 😒');
+
+
+// Truthy / Faslsy Value
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+// const money = 100;
+// if (money) {
+//     console.log("DOnt't spend it all ;)");
+// } else {
+//     console.log('You should get a job!');
+// }
+
+// let height = 1;
+// if (height) {
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDERFINED ');
+// }
+
+
+const age = '18';
+if (age === 18) console.log('You just become an adult(Strict)');
+
+if (age == 18) console.log('You just become an adult(loose)');
+
+const favorite = prompt("WHat's your favorite number?");
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite == 22) {
+    console.log('22 is your favorite number');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
